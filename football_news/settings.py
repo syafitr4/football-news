2ebfb7b84aa8268fd5fab09e78ab140e4f4aaa7a
syafitr4 <daffasyafitra12@gmail.com>
2025-09-10 16:43:03 +0700

    tutorial 2

--- football_news/settings.py
+++ football_news/settings.py
@@ -31,6 +31,9 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","daffa-syafitra-footballnews.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://daffa-syafitra-footballnews.pbp.cs.ui.ac.id"
+]
 
 # Application definition
 
@@ -59,7 +62,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
